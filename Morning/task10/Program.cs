871d03896530dcd46ddda5f16cfe5dd197adf965
Дарья <dariaabdullina000@gmail.com>
2024-12-04 14:36:25 +0300

    Add files via upload
    Напишите программу которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
    Напишите программу которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
    Напишите программу, которая будет приниматьна вход два числа выыодить, является ли второе число кратным первому.
    если второе число некратно первому, то программа выводит остаток от деления.

---
+++ Morning/task10/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+9. Напишите программу которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98
+*/ 
+
+int number = new Random().Next(100, 1000);
+System.Console.WriteLine(number);
+
+int first = number / 100;
+int last  = number % 10;
+if(first > last)
+
+System.Console.WriteLine($"{first}{last}" );
+//System.Console.WriteLine(last);
+ 
